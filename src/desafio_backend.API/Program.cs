c245bab9f68852da90ee5e91bfc999597923ff5f
Eduardo Franco <franc0edu24@gmail.com>
2024-05-26 18:20:04 -0300

    feat: :construction: Middleware to translate response messages
    Middleware to translate response messages
    
    Middleware to translate response messages

--- src/desafio_backend.API/Program.cs
+++ src/desafio_backend.API/Program.cs
@@ -1,3 +1,6 @@
+using desafio_backend.API.Filter;
+using desafio_backend.API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHealthChecks();
@@ -6,7 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
 var app = builder.Build();
+
+// Translate middleware
+app.UseMiddleware<CultureMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
--- src/desafio_backend.Exception/ResourceErrorMessage.Designer.cs
+++ src/desafio_backend.Exception/ResourceErrorMessage.Designer.cs
@@ -22,7 +22,7 @@ namespace desafio_backend.Exception {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ResourceErrorMessage {
+    public class ResourceErrorMessage {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace desafio_backend.Exception {
         ///   Retorna a instância de ResourceManager armazenada em cache usada por essa classe.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("desafio_backend.Exception.ResourceErrorMessage", typeof(ResourceErrorMessage).Assembly);
@@ -51,12 +51,66 @@ namespace desafio_backend.Exception {
         ///   pesquisas de recursos que usam essa classe de recurso de tipo de alta segurança.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Saldo insuficiente..
+        /// </summary>
+        public static string INSUFFICIENT_FUNDS {
+            get {
+                return ResourceManager.GetString("INSUFFICIENT_FUNDS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a O pagamento não é válido..
+        /// </summary>
+        public static string PAYMENT_TYPE_NOT_VALID {
+            get {
+                return ResourceManager.GetString("PAYMENT_TYPE_NOT_VALID", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Você não é autorizado para realizar transferência.
+        /// </summary>
+        public static string UNAUTHORIZED_TO_TRANSFER {
+            get {
+                return ResourceManager.GetString("UNAUTHORIZED_TO_TRANSFER", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Erro desconhecido..
+        /// </summary>
+        public static string UNKNOWN_ERROR {
+            get {
+                return ResourceManager.GetString("UNKNOWN_ERROR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Destinatário desconhecido..
+        /// </summary>
+        public static string UNKNOWN_RECIPIENT {
+            get {
+                return ResourceManager.GetString("UNKNOWN_RECIPIENT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a O valor de transferência precisa ser maior que zero..
+        /// </summary>
+        public static string VALUE_GREATHER_THAN_ZERO {
+            get {
+                return ResourceManager.GetString("VALUE_GREATHER_THAN_ZERO", resourceCulture);
             }
         }
     }
