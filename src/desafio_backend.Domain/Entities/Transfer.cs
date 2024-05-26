2734368e957103a73d95799498906750529f58a4
Eduardo Franco <franc0edu24@gmail.com>
2024-05-26 19:46:17 -0300

    feat: :construction_worker: entities
    entities
    
    entities

---
+++ src/desafio_backend.Domain/Entities/Transfer.cs
@@ -0,0 +1,15 @@
+﻿using desafio_backend.Domain.Enums;
+
+namespace desafio_backend.Domain;
+
+public class Transfer
+{
+  public long TransferId { get; set; }
+  public long Payer { get; set; }
+  public long Payee { get; set; }
+  public decimal Value { get; set; }
+  public DateTime TransferDate { get; set; }
+  public PaymentType PaymentType { get; set; } 
+  public virtual User PayerUser { get; set; }
+  public virtual User PayeeUser { get; set; }
+}
---
+++ src/desafio_backend.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using desafio_backend.Domain.Enums;
+
+namespace desafio_backend.Domain;
+
+public class User
+{
+  public long UserId { get; set; } 
+  public string Name { get; set; } = string.Empty;
+  public string Email { get; set; } = string.Empty;
+  public string CpnjCpf { get; set; } = string.Empty;
+  public AccountType AccountType { get; set; }
+  public decimal Amount { get; set; } 
+  public string Password { get; set; } = string.Empty;
+  public DateTime CreatedAt { get; set; }
+}
