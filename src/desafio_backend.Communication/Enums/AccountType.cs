1d1daeec27ae1751ecd67b7fc05369ab385d08a4
Eduardo Franco <franc0edu24@gmail.com>
2024-05-26 18:19:06 -0300

    feat: :construction: enums of account and payment
    enums of account and payment
    
    enums of account and payment

---
+++ src/desafio_backend.Communication/Enums/AccountType.cs
@@ -0,0 +1,6 @@
+﻿namespace desafio_backend.Communication.Enums;
+public enum AccountType
+{
+    Shopkeeper = 0,
+    CommonUser = 1,
+}
---
+++ src/desafio_backend.Domain/Enums/AccountType.cs
@@ -0,0 +1,6 @@
+﻿namespace desafio_backend.Domain.Enums;
+public enum AccountType
+{
+    Shopkeeper = 0,
+    CommonUser = 1,
+}
